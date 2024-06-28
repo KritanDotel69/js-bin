757aaea88eb55dbb6472c9c572def9d5a041ca72
Kritan Dotel <dotelkritan@gmail.com>
2024-06-28 22:34:29 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Header from "./components/common/Header";
 import CodingSection from "./components/CodingSection/CodingSection";
 
 const App = () => {
   return (
     <>
 
-      <Header />
+
       <CodingSection />
 
 
--- src/components/CodingSection/CodeBlock.jsx/ConsoleBlock.jsx
+++ src/components/CodingSection/CodeBlock.jsx/ConsoleBlock.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { executeCode } from "../../../features/api";
 
-const ConsoleBlock = () => {
+
+const ConsoleBlock = ({ jsValue }) => {
+
+  const [output, setOutput] = useState(null);
+  console.log('console', output)
+
+
+  const onButtonClick = () => setOutput(null)
+
+  useEffect(() => {
+    const runCode = async () => {
+      if (!jsValue) return;
+      try {
+        const { run } = await executeCode(jsValue);
+        return setOutput(run);
+
+      } catch (error) {
+        console.error("Error executing code:", error)
+      }
+      setOutput('<p>Error</p>')
+    }
+    runCode()
+  }, [jsValue])
+
+
   return (
     <div>
-      <h1 className="output-head">Console</h1>
+      <div className="pr-5 flex flex-col md:flex-row items-baseline justify-between">
+        <h1 className="output-head">Console</h1>
+        <button onClick={onButtonClick} className="px-2 text-[12px] bg-primary border border-primary-dark rounded-md">Clear</button>
+      </div>
+      <iframe srcDoc={output?.stdout} frameborder="0"></iframe>
     </div>
   )
 }
